7fdd5750fbf48a1f604fb2a8f2acb9b72cdfd306
sheetalkumari1411 <sheetalchoudhary1411@gmail.com>
2026-04-30 11:01:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.map_visualization import create_map
+from map_visualization import create_map
 
 # -----------------------------
 # PAGE CONFIG
