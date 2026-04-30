c8bc41ee068fcbe1bc65689ea6e327d29c50231b
sheetalkumari1411 <sheetalchoudhary1411@gmail.com>
2026-04-30 10:52:35 +0530

    Refactor streamlit_app.py for improved structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.map_visualization import create_map
 
 # -----------------------------
